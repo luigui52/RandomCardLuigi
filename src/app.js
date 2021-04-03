2527aa0cd96d70f5843b236ce45de56e9e2ce959
luigui52 <luigui52@gmail.com>
2021-04-03 00:52:49 +0000

    Exercise complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  setInterval(() => {
+    //write your code here
+    document.querySelector(".card").classList.remove(generateRandomSuit());
+    document.querySelector(".card").classList.add(generateRandomSuit());
+    document.querySelector(".card").innerHTML = generateRandomNumber();
+  }, 1000);
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "🃏"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♦️", "♠️", "♥️", "♣️", "🃏"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
